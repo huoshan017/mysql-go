4cc73f7171bdeb0ae22198721ce8493b6e2bfcff
huoshan017 <huoshan017@github.com>
2019-03-29 01:02:59 +0800

    11111

--- base/table.go
+++ base/table.go
@@ -32,6 +32,40 @@ func (this *Database) LoadTable(tab *TableConfig) bool {
 	return true
 }
 
+func (this *Database) DropTable(table_name string) bool {
+	args := []interface{}{table_name}
+	if !this.ExecWith("DROP TABLE ?", args, nil, nil) {
+		return false
+	}
+	return true
+}
+
+func _get_field_type_str(field *FieldConfig) (field_type_str string) {
+	field_type_str = strings.ToUpper(field.Type)
+	field_type, o := GetMysqlFieldTypeByString(field_type_str)
+	if !o {
+		log.Printf("Cant get field %v type string", field.Name)
+		return ""
+	}
+
+	if field_type == MYSQL_FIELD_TYPE_DATE || field_type == MYSQL_FIELD_TYPE_DATETIME || (field_type == MYSQL_FIELD_TYPE_TIMESTAMP && field.Length == MYSQL_FIELD_DEFAULT_LENGTH) {
+		return
+	}
+
+	if field.Length == MYSQL_FIELD_DEFAULT_LENGTH {
+		default_length, o := GetMysqlFieldTypeDefaultLength(field_type)
+		if !o {
+			log.Printf("Cant get field type %v default length", field_type)
+			return ""
+		}
+		field_type_str = fmt.Sprintf("%v(%v)", field_type_str, default_length)
+	} else {
+		field_type_str = fmt.Sprintf("%v(%v)", field_type_str, field.Length)
+	}
+
+	return
+}
+
 func _get_field_create_flags_str(create_flags string) (create_flags_str string) {
 	flags := strings.Split(create_flags, ",")
 	for _, s := range flags {
@@ -67,14 +101,20 @@ func (this *Database) add_field(table_name string, field *FieldConfig) bool {
 	}
 
 	if result.rows != nil || result.Get() {
-		log.Printf("describe get rows not empty, no need to add field %v", field.Name)
+		log.Printf("describe get rows not empty, no need to add field %v\n", field.Name)
 		return true
+	}
+
+	field_type_str := _get_field_type_str(field)
+	if field_type_str == "" {
+		return false
 	}
 
 	create_flags_str := _get_field_create_flags_str(field.CreateFlags)
 
-	sql_str = fmt.Sprintf("ALTER TABLE `%s` ADD COLUMN `%s` %s %s", table_name, field.Name, field.Type, create_flags_str)
+	sql_str = fmt.Sprintf("ALTER TABLE `%s` ADD COLUMN `%s` %s %s", table_name, field.Name, field_type_str, create_flags_str)
 	if !this.Exec(sql_str, nil, nil) {
+		log.Printf("create table %v field %v failed\n", table_name, field.Name)
 		return false
 	}
 
@@ -108,11 +148,47 @@ func (this *Database) add_field(table_name string, field *FieldConfig) bool {
 				sql_str = fmt.Sprintf("ALTER TABLE `%s` AND UNIQUE (`%s`)", table_name, field.Name)
 			}
 		} else if index_type == MYSQL_INDEX_TYPE_FULLTEXT {
-			log.Printf("table %v field %v index type FULLTEXT not supported", table_name, field.Name)
+			log.Printf("table %v field %v index type FULLTEXT not supported\n", table_name, field.Name)
 		} else {
 
 		}
+
+		if !this.Query(sql_str, nil) {
+			log.Printf("create table %v field %v index failed\n", table_name, field.Name)
+			return false
+		}
 	}
 
+	return true
+}
+
+func (this *Database) remove_field(table_name, field_name string) bool {
+	args := []interface{}{table_name, field_name}
+	if !this.ExecWith("ALTER TABLE ? DROP COLUMN ?", args, nil, nil) {
+		return false
+	}
+	return true
+}
+
+func (this *Database) rename_field(table_name, old_field_name, new_field_name string) bool {
+	args := []interface{}{table_name, old_field_name, new_field_name}
+	if !this.ExecWith("ALTER TABLE ? CHANGE ? ?", args, nil, nil) {
+		return false
+	}
+	return true
+}
+
+func (this *Database) modify_field_attr(table_name string, field *FieldConfig) bool {
+	field_type_str := _get_field_type_str(field)
+	if field_type_str == "" {
+		log.Printf("get table %v field %v type string failed", table_name, field.Name)
+		return false
+	}
+	field_create_str := _get_field_create_flags_str(field.CreateFlags)
+	args := []interface{}{table_name, field.Name, field_type_str, field_create_str}
+	if !this.ExecWith("ALTER TABLE ? MODIFY ? ? ?", args, nil, nil) {
+		log.Printf("modify table %v field %v attr failed", table_name, field.Name)
+		return false
+	}
 	return true
 }
