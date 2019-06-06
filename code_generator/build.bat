29a20fefbad422351373eb6f6b91fe0724386705
huoshan017 <huoshan017@hotmail.com>
2019-06-06 17:28:57 +0800

    11111

--- code_generator/build.bat
+++ code_generator/build.bat
@@ -1,11 +1,3 @@
 set GOPATH=%cd%/../../../../../
 
 go build -i -o ../bin/code_generator.exe github.com/huoshan017/mysql-go/code_generator
-if errorlevel 1 goto exit
-if errorlevel 0 goto ok
-
-:exit
-echo build failed !!!
-
-:ok
-echo build ok
