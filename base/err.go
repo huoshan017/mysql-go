8ee98ded3e1fa4162c57f4db4b151ea739d78032
huoshan017 <huoshan017@hotmail.com>
2019-06-17 13:09:34 +0800

    11111

---
+++ base/err.go
@@ -0,0 +1,12 @@
+package mysql_base
+
+import (
+	"errors"
+)
+
+var (
+	ErrArgumentInvalid       = errors.New("argument invalid")
+	ErrQueryResultEmpty      = errors.New("query row result is empty")
+	ErrPrimaryFieldNotDefine = errors.New("primary field not defined")
+	ErrInternal              = errors.New("internal error")
+)
