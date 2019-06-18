33e01ace105e5c96617ed9e3a9a82180baaf3528
huoshan017 <huoshan017@hotmail.com>
2019-06-18 18:55:05 +0800

    11111

---
+++ generate/gen_cache_mgr.go
@@ -0,0 +1,15 @@
+package mysql_generate
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/huoshan017/mysql-go/base"
+)
+
+func gen_procedure_proxy_source(f *os.File, pkg_name string, table *mysql_base.TableConfig) string {
+	var str string
+	return str
+}
