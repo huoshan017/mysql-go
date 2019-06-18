c75314be87118999ebfa6f90c1174ed00703b844
huoshan017 <huoshan017@github.com>
2019-06-19 03:07:39 +0800

    11111

--- generate/gen_cache_mgr.go
+++ generate/gen_cache_mgr.go
@@ -9,7 +9,11 @@ import (
 	"github.com/huoshan017/mysql-go/base"
 )
 
-func gen_procedure_proxy_source(f *os.File, pkg_name string, table *mysql_base.TableConfig) string {
+func gen_cache_mgr_source(f *os.File, pkg_name string, table *mysql_base.TableConfig) string {
 	var str string
+
+	struct_row_name := _upper_first_char(table.Name)
+	cache_mgr_name := struct_row_name + "CacheMgr"
+
 	return str
 }
