4186f4e3d5d39f43103a85666e2af69b452b66b7
huoshan017 <huoshan017@github.com>
2019-08-22 01:34:03 +0800

    11111

--- proxy/test/build.sh
+++ proxy/test/build.sh
@@ -6,6 +6,6 @@ export PATH=$PATH:$GOPATH/bin
 go get -u -v -t github.com/golang/protobuf/protoc-gen-go
 
 cd ../../bin
-./code_generator -c ../proxy/test/db_define/game_db.json -d ../proxy/test -p ../example/protobuf/protoc.exe
+./code_generator -c ../proxy/test/db_define/game_db.json -d ../proxy/test -p ../example/protobuf/protoc
 
 go build -i -o ../bin/test_client github.com/huoshan017/mysql-go/proxy/test
