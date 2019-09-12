2122eb429ed996bddb1e8b4ca8a4a050caaedd50
huoshan017 <huoshan017@github.com>
2019-09-12 20:10:41 +0800

    11111

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+go build github.com/huoshan017/mysql-go/base
+go build github.com/huoshan017/mysql-go/manager
+go build github.com/huoshan017/mysql-go/generate
+
+cd code_generator
+bash build.sh
+cd ..
+
+cd proxy
+bash build.sh
+cd ..
--- code_generator/build.sh
+++ code_generator/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 export GOPATH=$(pwd)/../../../..
-echo $GOPATH
 go get -v -u -t github.com/go-sql-driver/mysql
-go build -i -o bin/code_generator github.com/huoshan017/mysql-go/code_generator
+go build -i -o ../bin/code_generator github.com/huoshan017/mysql-go/code_generator
