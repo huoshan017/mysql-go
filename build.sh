4fdcbdb5522d9dbecf266ce3cf3eecb222f697f2
huoshan017 <huoshan017@github.com>
2019-09-12 20:22:27 +0800

    11111

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go build github.com/huoshan017/mysql-go/base
-go build github.com/huoshan017/mysql-go/manager
-go build github.com/huoshan017/mysql-go/generate
+#go build github.com/huoshan017/mysql-go/base
+#go build github.com/huoshan017/mysql-go/manager
+#go build github.com/huoshan017/mysql-go/generate
 
 cd code_generator
 bash build.sh
