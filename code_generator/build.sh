d925081ab59ab3698e4003ef460f33698e584eaa
huoshan017 <huoshan017@github.com>
2019-09-12 20:41:42 +0800

    11111

--- code_generator/build.sh
+++ code_generator/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export GOPATH=$(pwd)/../../../..
+export GOPATH=$(pwd)/../../../../..
 go get -v -u github.com/go-sql-driver/mysql
+go get -v -u github.com/hashicorp/golang-lru/simplelru
 go build -i -o ../bin/code_generator github.com/huoshan017/mysql-go/code_generator
